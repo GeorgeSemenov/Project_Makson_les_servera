f253347ae910a469eabcde588dd2ffe9bd176d29
GeorgeSemenov <zhora.sem@yandex.ru>
2021-09-15 11:33:20 +0300

    last commit

--- source/blocks/header/header.js
+++ source/blocks/header/header.js
@@ -1,2 +1,23 @@
 $(document).ready(function(){
+  let header = $('.header')
+  let navigation = $('.header__navigation');
+  let headerVisiblePartlHeight = '56px';
+  let headerHeight;
+  //для планшетных и мобильных экранов убираем навигацию нафиг
+  if ($(window).width() < 1199){
+    headerHeight = header.css('height');
+    header.css({'height': headerVisiblePartlHeight});
+  }
+
+  //При нажатии на кнопку сэндвича
+  $(".header__sandwich-button").click(function(){
+    header.toggleClass('header_expanded')
+
+    if(header.hasClass('header_expanded') && $(window).width() < 1199){
+      header.css({'height': headerHeight})
+    }
+    else{
+      header.css({'height': headerVisiblePartlHeight});
+    }
+  })
 })
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,3 +1,5 @@
 import './index.scss';
 import '../../JS-components/libs.js';
 import '../../vendor/nice-select/jquery-nice-select-1.1.0/js/jquery.nice-select.min.js';
+
+import '../../blocks/header/header.js';
