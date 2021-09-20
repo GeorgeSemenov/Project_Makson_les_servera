46e623d0f7f5f6e49bda617f81ffad4f16393063
GeorgeSemenov <zhora.sem@yandex.ru>
2021-09-20 11:16:57 +0300

    header almost ready

--- source/blocks/header/header.js
+++ source/blocks/header/header.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
   let header = $('.header')
   let navigation = $('.header__navigation');
-  let headerVisiblePartlHeight = '56px';
+  let headerVisiblePartlHeight = '51px';
   let headerHeight;
   //для планшетных и мобильных экранов убираем навигацию нафиг
   if ($(window).width() < 1199){
     headerHeight = header.css('height');
     header.css({'height': headerVisiblePartlHeight});
+    header.siblings('main').css({'padding-top': headerVisiblePartlHeight});
   }
+
+  window.addEventListener('resize', function(event) {
+    console.log("shut up");
+  });
 
   //При нажатии на кнопку сэндвича
   $(".header__sandwich-button").click(function(){
